dfbcc9b8352d2c101f1f963fd537e38c3d87217a
shakh <shakhriyor315@gmail.com>
2023-01-18 16:02:35 +0500

    create widget fo big texts

---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+
+class BigText extends StatelessWidget {
+  final Color? color;
+  final String text;
+  double size;
+  TextOverflow overFlow;
+
+  BigText({
+    Key? key,
+    this.color,
+    required this.text,
+    this.size = 20,
+    this.overFlow = TextOverflow.ellipsis,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: overFlow,
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+        fontSize: size,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+}
