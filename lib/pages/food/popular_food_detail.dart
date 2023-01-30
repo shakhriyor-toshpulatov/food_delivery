53926a2ebd48289992ca2767d852a3674512e2f2
shakh <shakhriyor315@gmail.com>
2023-01-30 11:22:02 +0500

    не знаю что натворил

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -18,151 +18,149 @@ class PopularFoodDetail extends StatelessWidget {
     Get.find<PopularProductController>()
         .initProduct(Get.find<CartController>());
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Stack(
-          children: [
-            // background image
-            Positioned(
-              left: 0,
-              right: 0,
-              child: Container(
-                width: double.maxFinite,
-                height: Dimensions.popularFoodImageSize,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage(
-                          "assets/image/food2.png",
-                        ))),
-              ),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          // background image
+          Positioned(
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.maxFinite,
+              height: Dimensions.popularFoodImageSize,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                        "assets/image/food2.png",
+                      ))),
             ),
-            //icon widgets
-            Positioned(
-              top: Dimensions.height45,
-              left: Dimensions.width20,
-              right: Dimensions.width20,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ),
+          //icon widgets
+          Positioned(
+            top: Dimensions.height45,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                    onTap: () {
+                      Get.to(() => MainFoodPage());
+                    },
+                    child: AppIcon(icon: Icons.arrow_back)),
+                AppIcon(icon: Icons.shopping_cart_outlined),
+              ],
+            ),
+          ),
+          //introduce of food
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: Dimensions.popularFoodImageSize - 20,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+                top: Dimensions.height20,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(Dimensions.radius20),
+                  topLeft: Radius.circular(Dimensions.radius20),
+                ),
+                color: Colors.white,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  GestureDetector(
-                      onTap: () {
-                        Get.to(() => MainFoodPage());
-                      },
-                      child: AppIcon(icon: Icons.arrow_back)),
-                  AppIcon(icon: Icons.shopping_cart_outlined),
+                  AppColumn(text: "Fast Food"),
+                  SizedBox(height: Dimensions.height20),
+                  BigText(text: "Introduce"),
+                  SizedBox(height: Dimensions.height20),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: ExpandableTextWidget(
+                          text:
+                              """It is not a secret that our meals influence much on our health and our mood. That's why I consider breakfast to be the most important meal in the day as it is the first our meal and it can put us in good spirits."""),
+                    ),
+                  ),
                 ],
               ),
             ),
-            //introduce of food
-            Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              top: Dimensions.popularFoodImageSize - 20,
-              child: Container(
-                padding: EdgeInsets.only(
-                  left: Dimensions.width20,
-                  right: Dimensions.width20,
+          ),
+        ],
+      ),
+      bottomNavigationBar:
+          GetBuilder<PopularProductController>(builder: (popularProduct) {
+        return Container(
+          height: Dimensions.bottomHeightBar,
+          padding: EdgeInsets.only(
+            top: Dimensions.height20,
+            bottom: Dimensions.height20,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+          ),
+          decoration: BoxDecoration(
+            color: AppColors.buttonBackgroundColor,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(Dimensions.radius20 * 2),
+                topRight: Radius.circular(Dimensions.radius20 * 2)),
+          ),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Container(
+              padding: EdgeInsets.only(
                   top: Dimensions.height20,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(Dimensions.radius20),
-                    topLeft: Radius.circular(Dimensions.radius20),
-                  ),
-                  color: Colors.white,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    AppColumn(text: "Fast Food"),
-                    SizedBox(height: Dimensions.height20),
-                    BigText(text: "Introduce"),
-                    SizedBox(height: Dimensions.height20),
-                    Expanded(
-                      child: SingleChildScrollView(
-                        child: ExpandableTextWidget(
-                            text:
-                                """It is not a secret that our meals influence much on our health and our mood. That's why I consider breakfast to be the most important meal in the day as it is the first our meal and it can put us in good spirits."""),
-                      ),
-                    ),
-                  ],
-                ),
+                  bottom: Dimensions.height20,
+                  left: Dimensions.width20,
+                  right: Dimensions.width20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Dimensions.radius20),
+                  color: Colors.white),
+              child: Row(
+                children: [
+                  GestureDetector(
+                      onTap: () {
+                        popularProduct.setQuantity(false);
+                      },
+                      child: Icon(Icons.remove, color: AppColors.signColor)),
+                  SizedBox(width: Dimensions.width10 / 2),
+                  BigText(text: popularProduct.quantity.toString()),
+                  SizedBox(width: Dimensions.width10 / 2),
+                  GestureDetector(
+                      onTap: () {
+                        popularProduct.setQuantity(true);
+                      },
+                      child: Icon(Icons.add, color: AppColors.signColor)),
+                ],
               ),
             ),
-          ],
-        ),
-        bottomNavigationBar:
-            GetBuilder<PopularProductController>(builder: (popularProduct) {
-          return Container(
-            height: Dimensions.bottomHeightBar,
-            padding: EdgeInsets.only(
-              top: Dimensions.height20,
-              bottom: Dimensions.height20,
-              left: Dimensions.width20,
-              right: Dimensions.width20,
+            Container(
+              padding: EdgeInsets.only(
+                top: Dimensions.height20,
+                bottom: Dimensions.height20,
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  // popularProduct.addItem(product);
+                },
+                child: BigText(
+                  text: "\$10 | Add to cart",
+                  color: Colors.white,
+                ),
+              ),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Dimensions.radius20),
+                  color: AppColors.mainColor),
             ),
-            decoration: BoxDecoration(
-              color: AppColors.buttonBackgroundColor,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(Dimensions.radius20 * 2),
-                  topRight: Radius.circular(Dimensions.radius20 * 2)),
-            ),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(
-                        top: Dimensions.height20,
-                        bottom: Dimensions.height20,
-                        left: Dimensions.width20,
-                        right: Dimensions.width20),
-                    decoration: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.circular(Dimensions.radius20),
-                        color: Colors.white),
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                            onTap: () {
-                              popularProduct.setQuantity(false);
-                            },
-                            child:
-                                Icon(Icons.remove, color: AppColors.signColor)),
-                        SizedBox(width: Dimensions.width10 / 2),
-                        BigText(text: popularProduct.quantity.toString()),
-                        SizedBox(width: Dimensions.width10 / 2),
-                        GestureDetector(
-                            onTap: () {
-                              popularProduct.setQuantity(true);
-                            },
-                            child: Icon(Icons.add, color: AppColors.signColor)),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(
-                      top: Dimensions.height20,
-                      bottom: Dimensions.height20,
-                      left: Dimensions.width20,
-                      right: Dimensions.width20,
-                    ),
-                    child: GestureDetector(
-                      onTap: () {
-                        // popularProduct.addItem(product);
-                      },
-                      child: BigText(
-                        text: "\$10 | Add to cart",
-                        color: Colors.white,
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.circular(Dimensions.radius20),
-                        color: AppColors.mainColor),
-                  ),
-                ]),
-          );
-        }));
+          ]),
+        );
+      }
+      ),
+    );
   }
 }
